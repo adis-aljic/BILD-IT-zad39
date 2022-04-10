556ee560095d2b132a25e25479607be638747c26
adis-aljic <adis.qm@gmail.com>
2022-04-10 23:11:57 +0200

    bild it

--- adisAljicChallenge05Zadatak39.js
+++ adisAljicChallenge05Zadatak39.js
@@ -33,13 +33,3 @@ if ((broj1>broj2)&&(broj1<broj3)&&(broj2<broj3) )
 else {
     console.log("Tri cijela broja: "+broj1+","+broj2+" i "+broj3+ " u rastucem obliku izgledaju ovako: " +broj2+" "+broj3+" "+broj1+".")
 }
-
-
-
-
-
-
-
-
-
-
